ead0b907c26b3b5cd7ca96b7631179385a496a28
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2022-11-09 23:52:37 +0800

    Add files via upload

---
+++ src/Global.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "version.h"
+
+#define ConfigFile "./plugins/DeathExplosion/config.json"
+
+#include <iostream>
+#include <llapi/LoggerAPI.h>
+#include <llapi/Global.h>
+#include <llapi/mc/Level.hpp>
+#include <llapi/mc/Vec3.hpp>
+#include <llapi/mc/Player.hpp>
+#include <llapi/EventAPI.h>
+
+#include "setting.h"
+
+extern Logger logger;
